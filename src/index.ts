628e8a469e5b3e9787fbc6e5df2f8064ea0850d7
ramazanyildirim <ramazan.yildirim@bromcom.com>
2023-10-05 18:00:06 +0300

    update some configs

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+
+import './badge/badge.component.js';
+import './avatar/avatar.component.js';
+
+export * from './avatar/avatar.component.js';
+export * from './badge/badge.component.js';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
