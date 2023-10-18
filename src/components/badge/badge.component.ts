e8dbcf8d8f872d6ed896b515487d8ef502bcc951
ramazanyildirim <ramazan.yildirim@bromcom.com>
2023-10-19 01:45:24 +0300

    badge structure v1

--- src/components/badge/badge.component.ts
+++ src/components/badge/badge.component.ts
@@ -2,51 +2,36 @@ import { html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import { PlusBase } from "../../base/plus-base";
 import { ColorHelper } from "../../helper/color-helper";
-import { badgeStyle } from "./badge.style";
+import { badgeStyle } from "./badge.style"
+;
 @customElement("plus-badge")
 export class BadgeComponent extends PlusBase {
-    @property({ type: String }) size: "sm" | "md" | "lg" = "md";
-    @property({ type: String }) kind: "dot" | "text" = "text";
-    @property({ type: Boolean, converter: value => value != "false" }) invert = false;
-    @property({ type: String }) placement: "top-left" | "top-right" | "bottom-left" | "bottom-right" = "top-right";
-    @property({ type: String }) content!: string;
-    @property({ type: String }) status!: "success" | "warning" | "error" | "info";
+  @property({ type: String }) size: "sm" | "md" | "lg" = "md";
+  @property({ type: String }) kind: "dot" | "text" = "text";
+  @property({ type: Boolean, converter: value => value != "false" }) invert = false;
+  @property({ type: String }) placement: "top-left" | "top-right" | "bottom-left" | "bottom-right" = "top-right";
+  @property({ type: String }) content!: string;
+  @property({ type: String }) status: "success" | "warning" | "error" | "info" | "default" = "default";
+
+  // next version of plus-badge
+  @property({ type: String }) color!: string;
+
+  render() {
+    const { size, color, invert, kind, status, placement, content } = this;
 
 
-    @property({ type: String }) color!: string;
-
-
-    render() {
-        const { size, color, invert } = this;
-
-        const { tone, colorName } = ColorHelper.parseColorString(color);
-
-        const dc = ColorHelper.generateColorVariables({
-            color: colorName,
-            tones: {
-                [tone]: ["bg"],
-            },
-            variable: "--plus-badge",
-        });
-
-        const style = {
-            ...dc,
-            ...(dc["--badge-bg"] && {
-                "--badge-text": ColorHelper.getContrastingTextColor(dc["--badge-bg"]),
-            }),
-        };
-
-        const { base } = badgeStyle({ size });
-        return html`
-            <div class="${base()} bg-blue-500" style="${ColorHelper.objectToCssString(style)}" role="status" aria-live="polite">
-                <slot></slot>
-            </div>
-        `;
-    }
+    const { base } = badgeStyle({ size, kind, status, invert, placement });
+    return html`
+      <div class="relative box-border inline-block" role="status" aria-live="polite">
+        <slot> </slot>
+        <div class=${base()}>${content}</div>
+      </div>
+    `;
+  }
 }
 
 declare global {
-    interface HTMLElementTagNameMap {
-        "plus-badge": BadgeComponent;
-    }
+  interface HTMLElementTagNameMap {
+    "plus-badge": BadgeComponent;
+  }
 }
--- src/components/badge/badge.style.ts
+++ src/components/badge/badge.style.ts
@@ -2,31 +2,114 @@ import { tv } from "tailwind-variants";
 
 export const badgeStyle = tv({
   slots: {
-    base: [
-      "inline-flex items-center justify-center",
-      "flex items-center justify-center flex-row",
-      "rounded-full text-center",
-      "bg-[--badge-bg] text-[--badge-text]",
-      "whitespace-nowrap",
-    ],
+    base: ["absolute font-sans inline-flex items-center justify-center text-center whitespace-nowrap z-auto"],
   },
   variants: {
     size: {
       sm: {
-        base: "text-xs min-w-1.5 min-h-1.5",
-        dotText: "text-xs px-1.5 min-w-4 p-3",
+        base: "text-xs w-4 h-4 p-0.5",
       },
       md: {
-        base: "text-sm min-w-2 min-h-2",
-        dotText: "px-1.5 min-w-5",
+        base: "text-xs py-1 empty:px-1 empty:min-w-0 min-w-6 empty:min-h-0 min-h-6",
       },
       lg: {
-        base: "text-base min-w-2.5 min-h-2.5",
-        dotText: "px-2 min-w-6",
+        base: "text-base w-6 h-6 p-2",
+      },
+    },
+    kind: {
+      dot: {
+        base: "rounded-full",
+      },
+      text: {
+        base: "rounded-full",
+      },
+    },
+    status: {
+      success: {
+        base: "bg-color-success text-color-base",
+      },
+      warning: {
+        base: "bg-color-warning text-color-base",
+      },
+      error: {
+        base: "bg-color-error text-color-base",
+      },
+      info: {
+        base: "bg-color-information text-color-base",
+      },
+      default: {
+        base: "bg-color-default text-color-default",
+      },
+    },
+    invert: {
+      true: {
+        base: "",
+      },
+    },
+    placement: {
+      "top-left": {
+        base: "-left-2 -top-2 empty:-left-1 empty:-top-1",
+      },
+      "top-right": {
+        base: "-right-2 -top-2 empty:-right-1 empty:-top-1",
+      },
+      "bottom-left": {
+        base: "top-auto -bottom-2 -left-2 empty:-bottom-1 empty:-left-1",
+      },
+      "bottom-right": {
+        base: "top-auto -right-2 -bottom-2 empty:-bottom-1 empty:-right-1",
       },
     },
   },
+  compoundVariants: [
+    {
+      status: ["success", "warning", "error", "info"],
+      invert: true,
+      class: {
+        base: ["text-color-default"],
+      },
+    },
+    {
+      status: "success",
+      invert: true,
+      class: {
+        base: ["bg-color-invert-success"],
+      },
+    },
+    {
+      status: "warning",
+      invert: true,
+      class: {
+        base: ["bg-color-invert-warning"],
+      },
+    },
+    {
+      status: "error",
+      invert: true,
+      class: {
+        base: ["bg-color-invert-error"],
+      },
+    },
+    {
+      status: "info",
+      invert: true,
+      class: {
+        base: ["bg-color-invert-information"],
+      },
+    },
+    {
+      status: "default",
+      invert: true,
+      class: {
+        base: ["bg-color-invert-default text-color-base"],
+      },
+    },
+  ],
   defaultVariants: {
     size: "md",
+    status: "default",
+    kind: "text",
+    placement: "top-right",
+    invert: false,
   },
 });
