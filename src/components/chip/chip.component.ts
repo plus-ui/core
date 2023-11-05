87fa62441a320ece33e44f445ad313ffe58ec092
Murat Polat Ozkan <muratpolatozkan@gmail.com>
2023-11-05 14:50:10 +0300

    chip style update

--- src/components/chip/chip.component.ts
+++ src/components/chip/chip.component.ts
@@ -8,14 +8,15 @@ export class ChipComponent extends PlusBase {
   @property({ type: String }) size: "sm" | "md" | "lg" = "md";
   @property({ type: String }) kind: "filled" | "outlined" = "filled";
   @property({ type: String }) status: "success" | "warning" | "error" | "info" | "default" = "default";
+  @property({ type: Boolean, converter: value => value != "false" }) invert = false;
 
   // next version of plus-badge
   @property({ type: String }) color!: string;
 
   render() {
-    const { size, disabled, status, kind } = this;
+    const { size, disabled, status, kind, invert } = this;
 
-    const { base } = chipStyle({ size, kind, status, disabled });
+    const { base } = chipStyle({ size, kind, status, disabled, invert });
     return html`
       <div class=${base()}>
         <slot></slot>
--- src/components/chip/chip.style.ts
+++ src/components/chip/chip.style.ts
@@ -2,27 +2,33 @@ import { tv } from "tailwind-variants";
 
 export const chipStyle = tv({
   slots: {
-    base: ["font-sans inline-flex items-center justify-center text-center whitespace-nowrap z-auto rounded-full gap-2 border border-solid"],
+    base: [
+      "font-sans inline-flex items-center justify-center text-center whitespace-nowrap z-auto rounded-fullborder border-solid rounded-full bg-color-default text-color-default",
+    ],
   },
   variants: {
     size: {
       sm: {
-        base: "text-xs px-1 py-0.5",
+        base: "text-xs px-1.5 py-0.5 gap-1",
       },
       md: {
-        base: "text-sm px-2 py-1",
+        base: "text-sm px-2 py-1 gap-2",
       },
       lg: {
-        base: "text-base px-3 py-2",
+        base: "text-base px-3 py-2 gap-2",
       },
     },
     kind: {
       filled: {
-        base: "rounded-full border-transparent",
+        base: "border-transparent",
       },
       outlined: {
-        base: "bg-transparent",
+        base: "bg-color-base",
       },
+    },
+    invert: {
+      true: "bg-color-invert-default text-color-base",
+      false: "",
     },
     status: {
       success: {
@@ -37,63 +43,104 @@ export const chipStyle = tv({
       info: {
         base: "bg-color-information text-color-base",
       },
-      default: {
-        base: "bg-color-default text-color-default",
-      },
+      default: {},
     },
     disabled: {
       true: {
-        base: "bg-color-disabled text-color-disabled cursor-not-allowed",
+        base: "",
       },
     },
   },
   compoundVariants: [
     {
+      invert: true,
+      status: "success",
+      class: {
+        base: ["bg-color-invert-success text-color-default"],
+      },
+    },
+    {
+      invert: true,
+      status: "info",
+      class: {
+        base: ["bg-color-invert-information text-color-default"],
+      },
+    },
+    {
+      invert: true,
+      status: "warning",
+      class: {
+        base: ["bg-color-invert-warning text-color-default"],
+      },
+    },
+    {
+      invert: true,
+      status: "error",
+      class: {
+        base: ["bg-color-invert-error text-color-default"],
+      },
+    },
+    {
       kind: "outlined",
       class: {
         base: ["border border-solid bg-transparent"],
-      }
+      },
     },
     {
       kind: "outlined",
       status: "info",
       class: {
         base: ["text-color-primary border-color-primary"],
-      }
+      },
     },
     {
       kind: "outlined",
       status: "success",
       class: {
         base: ["text-color-success border-color-success"],
-      }
+      },
     },
     {
       kind: "outlined",
       status: "warning",
       class: {
         base: ["text-color-warning border-color-warning"],
-      }
+      },
     },
     {
       kind: "outlined",
       status: "error",
       class: {
         base: ["text-color-error border-color-error"],
-      }
+      },
     },
     {
       kind: "outlined",
       status: "default",
       class: {
         base: ["text-color-default border-color-default"],
-      }
-    }
+      },
+    },
+    {
+      disabled: true,
+      kind: "filled",
+      class: {
+        base: ["bg-color-disabled text-color-disabled cursor-not-allowed"],
+      },
+    },
+    {
+      disabled: true,
+      kind: "outlined",
+      class: {
+        base: ["border-color-disabled text-color-disabled cursor-not-allowed"],
+      },
+    },
   ],
   defaultVariants: {
     size: "md",
     status: "default",
     kind: "filled",
     disabled: false,
+    invert: false,
   },
 });
