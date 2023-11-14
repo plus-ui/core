8843b43024bbe1e9cfcf8619b7032cf752e3b2ec
Murat Polat Ozkan <muratpolatozkan@gmail.com>
2023-11-15 00:14:04 +0300

    Added cem analyze to generate custom-element.json

---
+++ custom-elements-manifest.config.mjs
@@ -0,0 +1,44 @@
+// import { myAwesomePlugin } from 'awesome-plugin';
+// import { readmePlugin } from 'cem-plugin-readme';
+
+export default {
+  /** Globs to analyze */
+  globs: [
+    'src/components/**/*.component.ts'
+],
+  /** Globs to exclude   */
+//   exclude: ['src/foo.js'],
+  /** Directory to output CEM to */
+//   outdir: 'dist',
+  /** Run in dev mode, provides extra logging */
+//   dev: true,
+  /** Run in watch mode, runs on file changes */
+//   watch: true,
+  /** Include third party custom elements manifests */
+//   dependencies: true,
+  /** Output CEM path to `package.json`, defaults to true */
+  packagejson: false,
+  /** Enable special handling for litelement */
+  litelement: true,
+  /** Enable special handling for catalyst */
+  catalyst: false,
+  /** Enable special handling for fast */
+  fast: false,
+  /** Enable special handling for stencil */
+  stencil: false,
+  /** Provide custom plugins */
+//   plugins: [
+//     readmePlugin({
+//       header: 'README.head.md',
+//       footer: 'README.foot.md',
+//     }),
+//   ]
+
+//   /** Overrides default module creation: */
+//   overrideModuleCreation: ({ts, globs}) => {
+//     const program = ts.createProgram(globs, defaultCompilerOptions);
+//     const typeChecker = program.getTypeChecker();
+
+//     return program.getSourceFiles().filter(sf => globs.find(glob => sf.fileName.includes(glob)));
+//   },
+}
