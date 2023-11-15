a290fef572e5dcac9b6c8e8f1e828bd311a95d08
ramazanyildirim <ramazan.yildirim@bromcom.com>
2023-11-15 17:15:10 +0300

    update  config

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,47 +1,22 @@
-import { AlertComponent } from "./alert/alert.component";
-import { AvatarComponent } from "./avatar/avatar.component";
-import { BadgeComponent } from "./badge/badge.component";
-import { BreadComponentNavItem } from "./breadcrumb-item/breadcrumb-item.component";
-import { BreadCrumbComponent } from "./breadcrumb/breadcrumb.component";
-import { ButtonGroupComponent } from "./button-group/button-group.component";
-import { ButtonComponent } from "./button/button.component";
-import { CheckboxComponent } from "./checkbox/checkbox.component";
-import { ChipComponent } from "./chip/chip.component";
-import { DividerComponent } from "./divider/divider.component";
-import { InputComponent } from "./input/input.component";
-import { LinkComponent } from "./link/link.component";
-import { ProgressComponent } from "./progress/progress.component";
-import { RadioGroupComponent } from "./radio-group/radio-group.component";
-import { RadioComponent } from "./radio/radio.component";
-import { RatingComponent } from "./rating/rating.component";
-import { TabGroupComponent } from "./tab/tab-group.component";
-import { TabPanelComponent } from "./tab/tab-panel.component";
-import { TabComponent } from "./tab/tab.component";
-import { TextComponent } from "./text/text.component";
-import { textareaComponent } from "./textarea/textarea.component";
-import { ToggleComponent } from "./toggle/toggle.component";
-
-export {
-  AlertComponent as Alert,
-  AvatarComponent as Avatar,
-  BadgeComponent as Badge,
-  BreadCrumbComponent as Breadcrumb,
-  BreadComponentNavItem as BreadcrumbItem,
-  ButtonComponent as Button,
-  ButtonGroupComponent as ButtonGroup,
-  CheckboxComponent as Checkbox,
-  ChipComponent as Chip,
-  DividerComponent as Divider,
-  InputComponent as Input,
-  LinkComponent as Link,
-  ProgressComponent as Progress,
-  RadioComponent as Radio,
-  RadioGroupComponent as RadioGroup,
-  RatingComponent as Rating,
-  TabComponent as Tab,
-  TabGroupComponent as TabGroup,
-  TabPanelComponent as TabPanel,
-  TextComponent as Text,
-  textareaComponent as Textarea,
-  ToggleComponent as Toggle,
-};
+export * from "./avatar/avatar.component";
+export * from "./badge/badge.component";
+export * from "./button/button.component";
+export * from "./link/link.component";
+export * from "./divider/divider.component";
+export * from "./chip/chip.component";
+export * from "./alert/alert.component";
+export * from "./text/text.component";
+export * from "./button-group/button-group.component";
+export * from "./input/input.component";
+export * from "./textarea/textarea.component";
+export * from "./checkbox/checkbox.component";
+export * from "./radio/radio.component";
+export * from "./breadcrumb/breadcrumb.component";
+export * from "./breadcrumb-item/breadcrumb-item.component";
+export * from "./toggle/toggle.component";
+export * from "./radio-group/radio-group.component";
+export * from "./tab/tab.component";
+export * from "./tab/tab-group.component";
+export * from "./tab/tab-panel.component";
+export * from "./progress/progress.component";
+export * from "./rating/rating.component";
