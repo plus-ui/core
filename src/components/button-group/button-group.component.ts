c845de6ba3ffca006c246420d384ac72437d4da1
Murat Polat Ozkan <muratpolatozkan@gmail.com>
2023-11-16 03:31:58 +0300

    Unused prop declarations deleted.

--- src/components/button-group/button-group.component.ts
+++ src/components/button-group/button-group.component.ts
@@ -7,13 +7,6 @@ import { buttonGroupStyle } from "./button-group.style";
 export class ButtonGroupComponent extends PlusBase {
   @query(".button-group") buttonGroup!: HTMLElement;
 
-  @property({ type: String }) size: "sm" | "md" | "lg" = "md";
-  @property({ type: String }) kind: "dot" | "text" = "text";
-  @property({ type: Boolean, converter: value => value != "false" }) invert = false;
-  @property({ type: String }) content!: string;
-  @property({ type: String }) status: "success" | "warning" | "error" | "info" | "default" = "default";
-  @property({ type: String }) color!: string;
-
   @property({ type: String }) type: "vertical" | "horizontal" = "horizontal";
 
   handleSlotChange() {
