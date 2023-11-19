6bdd371b661e75aa5755ce26dc0d9d2e54d67a4a
Murat Polat Ozkan <muratpolatozkan@gmail.com>
2023-11-20 01:22:26 +0300

    radio group label, caption added and style updated

--- src/components/radio-group/radio-group.component.ts
+++ src/components/radio-group/radio-group.component.ts
@@ -1,12 +1,18 @@
 import { customElement, property, query } from "lit/decorators.js";
 import { html } from "lit/static-html.js";
 import { PlusBase } from "../../base/plus-base";
+import { captionStyle } from "../caption/caption.style";
+import { labelStyle } from "../label/label.style";
 import { radioGroupStyle } from "./radio-group.style";
 
 @customElement("plus-radio-group")
 export class RadioGroupComponent extends PlusBase {
   @property({ type: String }) aligment: "horizontal" | "vertical" = "horizontal";
   @query("slot") defaultSlot: HTMLSlotElement;
+  @property({ type: String }) size: "sm" | "md" | "lg" = "md";
+  @property({ type: String }) label?: string;
+  @property({ type: String }) caption?: string;
+  @property({ type: Boolean, converter: value => value != "false" }) error = false;
 
   private handleSlotChange() {
     const radios = [...this.defaultSlot?.assignedElements({ flatten: true })].filter(item => item.tagName.toLowerCase() === "plus-radio") as any[];
@@ -25,11 +31,17 @@ export class RadioGroupComponent extends PlusBase {
   }
 
   render() {
-    const { aligment } = this;
-    const { host } = radioGroupStyle({ aligment });
+    const { aligment, label, caption, size, error, required } = this;
+    const { host, inputWrapper } = radioGroupStyle({ aligment });
+    const LabelTemplate = () => (label ? html`<label class=${labelStyle({ size, required })} for="input">${label}</label>` : null);
+    const CaptionTemplate = () => (caption ? html`<div class=${captionStyle({ error, size })}>${caption}</div>` : null);
     return html`
       <div class=${host()}>
-        <slot class="radio-group" @slotchange=${this.handleSlotChange}> </slot>
+        ${LabelTemplate()}
+        <div class=${inputWrapper()}>
+          <slot class="radio-group" @slotchange=${this.handleSlotChange}> </slot>
+        </div>
+        ${CaptionTemplate()}
       </div>
     `;
   }
--- src/components/radio-group/radio-group.style.ts
+++ src/components/radio-group/radio-group.style.ts
@@ -1,21 +1,72 @@
 import { tv } from "tailwind-variants";
 
 export const radioGroupStyle = tv({
-    slots: {
-        host: ["antialiased font-sans text-base flex flex-row gap-2 items-start justify-start w-full relative"],
+  slots: {
+    host: ["font-sans antialiased flex flex-col  items-start justify-start w-full relative gap-0.5"],
+    inputWrapper: ["flex flex-row items-center justify-start gap-2 "],
+  },
+  variants: {
+    size: {
+      sm: "",
+      md: "",
+      lg: "",
     },
-    variants: {
-      aligment: {
-        horizontal: {
-          host: ["flex-row"],
-        },
-        vertical: {
-          host: ["flex-col"],
-        },
+
+    aligment: {
+      horizontal: {
+        inputWrapper: ["flex-row"],
+      },
+      vertical: {
+        inputWrapper: ["flex-col"],
       },
     },
-    compoundVariants: [],
-    defaultVariants: {
+  },
+  compoundVariants: [
+    {
+      size: "sm",
       aligment: "horizontal",
+      class: {
+        inputWrapper: ["gap-4"],
+      },
     },
-  });
+    {
+      size: "sm",
+      aligment: "vertical",
+      class: {
+        inputWrapper: ["gap-2"],
+      },
+    },
+    {
+      size: "md",
+      aligment: "horizontal",
+      class: {
+        inputWrapper: ["gap-5"],
+      },
+    },
+    {
+      size: "md",
+      aligment: "vertical",
+      class: {
+        inputWrapper: ["gap-2.5"],
+      },
+    },
+    {
+      size: "lg",
+      aligment: "horizontal",
+      class: {
+        inputWrapper: ["gap-6"],
+      },
+    },
+    {
+      size: "lg",
+      aligment: "vertical",
+      class: {
+        inputWrapper: ["gap-3"],
+      },
+    },
+  ],
+  defaultVariants: {
+    size: "md",
+    aligment: "horizontal",
+  },
+});
