d2d8ab042ea57e4d3be2c4933ee62b0d98d58e0f
ramazanyildirim <ramazan.yildirim@bromcom.com>
2024-05-04 22:49:59 +0300

    (bread-crumb):  link component has been integrated link component & removed bread-crumb-items &  updated styles and prop & update few configs.

--- src/components/link/link.style.ts
+++ src/components/link/link.style.ts
@@ -2,24 +2,45 @@ import { tv } from "tailwind-variants";
 
 export const linkStyle = tv({
   slots: {
-    base: [ 
-      "outline-none focus-visible:ring-2 focus-visible:ring-offset-2",
-      "transition-colors duration-200 ease-in-out",
-    ],
-
+    host: "flex flex-row items-center justify-center gap-2.5",
+    base: ["outline-none focus-visible:ring-2 focus-visible:ring-offset-2", "transition-colors duration-200 ease-in-out"],
   },
   variants: {
     kind: {
-      default: "text-color-link hover:underline visited:text-color-visited",
-      primary: "text-color-primary hover:underline visited:text-color-visited",
+      default: "text-color-link hover:underline ",
+      primary: "text-color-primary hover:underline",
     },
     disabled: {
       true: "cursor-not-allowed text-color-disabled user-select-none pointer-events-none",
       false: "",
     },
+    active: {
+      true: {
+        base: "visited:text-color-caption",
+      },
+      false: {
+        base: "text-color-link",
+      },
+    },
+    isBreadcrumb: {
+      true: "",
+      false: "",
+    },
   },
+  compoundVariants: [
+    {
+      kind: ["default", "primary"],
+      disabled: false,
+      isBreadcrumb: false,
+      class: {
+        base: "visited:text-color-visited",
+      },
+    },
+  ],
   defaultVariants: {
     kind: "default",
     disabled: false,
+    active: false,
+    isbreadCrumb: false,
   },
 });
