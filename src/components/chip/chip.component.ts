f9c0ba0c37fe1be8bd315113a8b92d08816cc095
Murat Polat Ozkan <muratpolatozkan@gmail.com>
2024-05-21 12:30:28 +0300

    feat(chip): add support for prefix icon

--- src/components/chip/chip.component.ts
+++ src/components/chip/chip.component.ts
@@ -9,16 +9,18 @@ export class ChipComponent extends PlusBase {
   @property({ type: String }) kind: "filled" | "outlined" = "filled";
   @property({ type: String }) status: "success" | "warning" | "error" | "info" | "default" = "default";
   @property({ type: Boolean, converter: value => value != "false" }) invert = false;
+  @property({ type: String, attribute: "prefix-icon" }) prefixIcon!: string;
 
   // next version of plus-badge
   @property({ type: String }) color!: string;
 
   render() {
-    const { size, disabled, status, kind, invert } = this;
+    const { size, disabled, status, kind, invert, prefixIcon } = this;
 
     const { base } = chipStyle({ size, kind, status, disabled, invert });
     return html`
       <div class=${base()}>
+        ${prefixIcon ? html`<i class="${prefixIcon}"></i>` : ""}
         <slot></slot>
       </div>
     `;
