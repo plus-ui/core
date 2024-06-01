7042748ed24e5430c7609b4eab88a3eef6a63a13
ramazanyildirim <ramazan.yildirim@bromcom.com>
2024-06-01 19:25:16 +0300

    feat(toast): new style & new props updates has been done.

--- src/components/toast/toast.component.ts
+++ src/components/toast/toast.component.ts
@@ -1,4 +1,4 @@
-import { PropertyValueMap, html } from "lit";
+import { PropertyValueMap, html, nothing } from "lit";
 import { customElement, property, query, state } from "lit/decorators.js";
 import { PlacementType, SizeType, StatusType } from "../../../src/model/plus-types";
 import { PlusBase } from "../../base/plus-base";
@@ -13,12 +13,19 @@ export class ToastComponent extends PlusBase {
 
   @property({ type: String }) placement: PlacementType = "top-start";
   @property({ type: String }) status: StatusType = "default";
-  @property({ type: Boolean, converter: value => value != "false" }) invert = false;
-  @property({ type: Boolean, converter: value => value != "false" }) dismiss = false;
+  @property({ type: Boolean, converter: value => value != "false" }) invert = true;
+  @property({ type: Boolean, converter: value => value != "false" }) dismiss = true;
+  @property({ type: Boolean, attribute: "action-buttons", converter: value => value != "false" }) actionButtons = false;
+  @property({ type: Boolean, converter: value => value != "false" }) statusIcon = true;
+
   @property({ type: Number }) duration: number = 3000;
   @property({ type: String }) position: "top-left" | "top-right" | "bottom-left" | "bottom-right" = "top-right";
 
   @property({ type: String }) message: string;
+  @property({ type: String }) header: string;
+
+  @property({ type: String }) cancelText: string = "Cancel";
+  @property({ type: String }) applyText: string = "Apply";
 
   @state() _icon = "";
   @property({ type: String })
@@ -32,7 +39,7 @@ export class ToastComponent extends PlusBase {
       warning: "fa-solid fa-triangle-exclamation",
       error: "fa-solid fa-circle-exclamation",
     };
-    return this._icon ? this._icon : defaultIcon[this.status];
+    return this._icon ? this._icon : this.statusIcon ? defaultIcon[this.status] : undefined;
   }
 
   @state() timeOutId;
@@ -76,17 +83,34 @@ export class ToastComponent extends PlusBase {
     }, this.duration);
   }
 
+  private close() {
+    this.emit("on-close");
+    this.toast.style.display = "none";
+  }
+
   render() {
-    const { size, invert, kind, status, placement, message, icon } = this;
-    const { base, iconClass, messageClass, closeClass } = toastStyle({ size, kind, status, invert, placement });
+    const { size, invert, kind, status, placement, message, icon, actionButtons, dismiss, header, cancelText, applyText } = this;
+    const { base, iconClass, messageClass, closeClass, contentClass, titleClass, footerClass, containerClass } = toastStyle({ size, kind, status, invert, placement });
 
     return html`
       <div class=${base()} id=${"toast-" + this.id} style=${"display:none"} @mouseenter=${this.mouseEnter} @mouseleave=${this.mouseLeave}>
-        <div class=${messageClass()}>
+        <div class=${containerClass()}>
           ${icon ? html`<i class=${iconClass() + " fa " + icon}></i>` : ""}
-          <span><slot>${message}</slot></span>
+          <div class=${contentClass()}>
+            ${header ? html`<div class=${titleClass()}>${header}</div>` : nothing} ${message ? html`<div class=${messageClass()}>${message}</div>` : nothing}
+            ${actionButtons
+              ? html`
+                  <div class=${footerClass()}>
+                    <plus-button .size=${size} kind="text" @plus-click=${() => this.close()}>${cancelText}</plus-button>
+                    <plus-button .size=${size} invert=${status === "default"} status=${status == "default" ? "invert" : status} @plus-click=${() => this.close()}
+                      >${applyText}</plus-button
+                    >
+                  </div>
+                `
+              : nothing}
+          </div>
+          ${dismiss ? html` <div class=${closeClass()} @click=${this.close}><i class="fa fa-solid fa-times"></i></div> ` : nothing}
         </div>
-        ${this.dismiss ? html`<div class=${closeClass()}><i class="fa fa-solid fa-times"></i></div>` : ""}
       </div>
     `;
   }
--- src/components/toast/toast.style.ts
+++ src/components/toast/toast.style.ts
@@ -3,10 +3,10 @@ import { tv } from "tailwind-variants";
 export const toastStyle = tv({
   slots: {
     base: [
-      "plus-toast-class font-sans antialiased flex items-center justify-between border border-solid border-transparent rounded font-medium w-full max-w-2xl transition duration-300 ease-in-out transform scale-95",
+      "plus-toast-class font-sans antialiased flex flex-row items-center justify-between border border-solid border-transparent rounded-lg font-medium w-full max-w-2xl transition duration-300 ease-in-out transform scale-95 shadow-lg",
     ],
     iconClass: ["plus-toast__icon"],
-    messageClass: ["plus-toast__message flex items-center"],
+    messageClass: ["plus-toast__message flex items-center font-normal text-color-default"],
     closeClass: [
       "plus-toast__close cursor-pointer relative",
       // Clicking area
@@ -15,20 +15,33 @@ export const toastStyle = tv({
       "before:w-10 before:h-10 before:-top-1.5 before:-left-3 before:z-10",
     ],
     toastContainer: ["plus-toast-container fixed z-50"],
+    contentClass: "flex flex-col",
+    titleClass: "font-medium text-color-default",
+    footerClass: "flex flex-row items-center justify-end gap-2",
+    containerClass: "plus-toast-container flex flex-row items-baseline justify-center flex-1",
   },
   variants: {
     size: {
       sm: {
-        base: ["p-5 gap-2.5 text-base"],
-        messageClass: ["gap-2.5"],
+        base: ["p-3"],
+        containerClass: "gap-2.5",
+        messageClass: "text-sm",
+        titleClass: "text-base",
+        contentClass: "gap-2",
       },
       md: {
-        base: ["p-6 gap-3 text-lg"],
-        messageClass: ["gap-3"],
+        base: ["p-3.5"],
+        containerClass: "gap-3",
+        titleClass: "text-lg",
+        messageClass: "text-base",
+        contentClass: "gap-2.5",
       },
       lg: {
-        base: ["p-7 gap-3.5 text-xl"],
-        messageClass: ["gap-3.5"],
+        base: ["p-4"],
+        containerClass: "gap-3.5",
+        messageClass: "text-lg",
+        titleClass: "text-xl",
+        contentClass: "gap-3",
       },
     },
     kind: {
@@ -37,23 +50,23 @@ export const toastStyle = tv({
     },
     status: {
       success: {
-        base: ["bg-color-success text-color-base"],
-        iconClass: ["text-color-invert-success"],
+        base: ["bg-color-invert-success text-color-default"],
+        iconClass: ["text-color-success"],
       },
       warning: {
-        base: ["bg-color-warning text-color-base"],
-        iconClass: ["text-color-invert-warning"],
+        base: ["bg-color-invert-warning text-color-default"],
+        iconClass: ["text-color-warning"],
       },
       error: {
-        base: ["bg-color-error text-color-base"],
-        iconClass: ["text-color-invert-error"],
+        base: ["bg-color-invert-error text-color-default"],
+        iconClass: ["text-color-error"],
       },
       info: {
-        base: ["bg-color-info text-color-base"],
-        iconClass: ["text-color-invert-info"],
+        base: ["bg-color-invert-info text-color-default"],
+        iconClass: ["text-color-info"],
       },
       default: {
-        base: ["bg-color-invert-default text-color-base"],
+        base: ["bg-color-default text-color-default"],
       },
     },
     invert: {
