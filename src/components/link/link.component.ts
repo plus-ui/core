34fd78f70f300eb7f2e463c40ea2528343ca0763
ramazanyildirim <ramazan.yildirim@bromcom.com>
2024-06-11 03:28:15 +0300

    feat(link): Added suffix icon

--- src/components/link/link.component.ts
+++ src/components/link/link.component.ts
@@ -19,22 +19,24 @@ export class LinkComponent extends PlusBase {
 
   @property({ type: Boolean, converter: value => value != "false" }) truncated = false;
   @property({ type: String, attribute: "prefix-icon" }) prefixIcon: string;
+  @property({ type: String, attribute: "suffix-icon" }) suffixIcon: string;
 
   private handleClick() {
     this.emit("plus-click");
   }
 
   render() {
-    const { href, target, download, rel, kind, disabled, size, active, isBreadcrumb, truncated, prefixIcon, readonly } = this;
+    const { href, target, download, rel, kind, disabled, size, active, isBreadcrumb, truncated, prefixIcon, readonly, suffixIcon } = this;
 
     const { base, host } = linkStyle({ kind, disabled, active, isBreadcrumb, readonly });
 
     return html`
       <div class=${host()}>
-        <plus-text size=${size} truncated=${truncated} class="w-full">
+        <plus-text size=${size} ?truncated=${truncated} class="w-full">
           <a class="${base()}" href="${href}" target="${ifDefined(target)}" download="${ifDefined(download)}" rel="${ifDefined(rel)}" @click="${this.handleClick}">
             ${prefixIcon ? html`<i class="${prefixIcon}"></i>` : ""}
             <slot></slot>
+            ${suffixIcon ? html`<i class="${suffixIcon}"></i>` : ""}
           </a>
         </plus-text>
         <slot class="no-underline" name="separator"></slot>
