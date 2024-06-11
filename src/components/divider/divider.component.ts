53a1d42b344bd098c3f42c8fb77060fd00269447
ramazanyildirim <ramazan.yildirim@bromcom.com>
2024-06-11 03:23:11 +0300

    feat(divider): Added  thickness property and updated some style props

--- src/components/divider/divider.component.ts
+++ src/components/divider/divider.component.ts
@@ -1,11 +1,13 @@
 import { html } from "lit/static-html.js";
 
 import { customElement, property } from "lit/decorators.js";
+import { styleMap } from "lit/directives/style-map.js";
 import { PlusBase } from "../../base/plus-base";
 import { DividerStyle } from "./divider.style";
+
 /** @ts-ignore */
-import style from "./divider.style.css?inline";
 import { unsafeCSS } from "lit";
+import style from "./divider.style.css?inline";
 
 @customElement("plus-divider")
 export class DividerComponent extends PlusBase {
@@ -13,13 +15,14 @@ export class DividerComponent extends PlusBase {
 
   @property({ type: String }) kind: "default" | "dashed" = "default";
   @property({ type: String }) orientation: "vertical" | "horizontal" = "horizontal";
+  @property({ type: Number }) thickness!: number;
 
   render() {
     const { kind, orientation } = this;
 
     const { base } = DividerStyle({ kind, orientation });
 
-    return html`<hr class="${base()}" />`;
+    return html`<hr class="${base()}" style=${styleMap({ "border-top-width": this.thickness + "px" })} aria-orientation=${orientation} role="separator" />`;
   }
 }
 
